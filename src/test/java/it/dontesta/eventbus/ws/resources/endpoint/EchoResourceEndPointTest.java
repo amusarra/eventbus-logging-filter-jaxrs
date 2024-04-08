f90ce63a9f0a343fffccd355462ddc85fdbec126
Antonio Musarra <antonio.musarra@gmail.com>
2024-04-09 00:31:21 +0200

    Step 2.4 - Realizzazione del Resource Endpoint

---
+++ src/test/java/it/dontesta/eventbus/ws/resources/endpoint/EchoResourceEndPointTest.java
@@ -0,0 +1,35 @@
+package it.dontesta.eventbus.ws.resources.endpoint;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class EchoResourceEndPointTest {
+
+  @Test
+  void testEchoSuccess() {
+    given()
+        .contentType(ContentType.JSON)
+        .body("{\"message\": \"Hello or Ciao, world!\"}")
+        .when().post("/api/rest/echo")
+        .then()
+        .statusCode(200)
+        .body(is("{\"message\": \"Hello or Ciao, world!\"}"));
+  }
+
+  @Test
+  void testEchoValidation() {
+    given()
+        .contentType(ContentType.JSON)
+        .body("{\"message\": \"Hello, world!\"}")
+        .when().post("/api/rest/echo")
+        .then()
+        .statusCode(400)
+        .body("title", is("Constraint Violation"));
+  }
+
+}
