d2e4c01e179b3304b3fa727059460c0b604f5188
Antonio Musarra <antonio.musarra@gmail.com>
2024-04-10 16:51:36 +0200

    Step 5 - Registrazione dei Consumer per gli eventi dell'Event Bus

---
+++ src/main/java/it/dontesta/eventbus/consumers/http/HttpResponseConsumer.java
@@ -0,0 +1,48 @@
+package it.dontesta.eventbus.consumers.http;
+
+import io.quarkus.runtime.StartupEvent;
+import io.vertx.mutiny.core.eventbus.EventBus;
+import io.vertx.mutiny.core.eventbus.Message;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+/**
+ * Questo consumer si registra all'indirizzo virtuale dell'evento per la risposta HTTP e gestisce
+ * gli eventi ricevuti dall'event bus tramite il metodo {@code handleEvent}.
+ *
+ * <p>La registrazione avviene all'avvio dell'applicazione tramite l'annotazione {@code @Observes}
+ * e il metodo {@code onStart}.
+ *
+ * <p>L'indicazione dell'indirizzo virtuale dell'evento per la richiesta HTTP è definita nel
+ * parametro di configurazione {@code app.eventbus.consumer.http.response.address} che viene
+ * iniettato tramite l'annotazione {@code @ConfigProperty}.
+ */
+
+@ApplicationScoped
+public class HttpResponseConsumer {
+
+  @Inject
+  EventBus eventBus;
+
+  @Inject
+  Logger log;
+
+  @ConfigProperty(name = "app.eventbus.consumer.http.response.address")
+  String httpResponseVirtualAddress;
+
+  void onStart(@Observes StartupEvent ev) {
+    log.debugf(
+        "Registering the consumers to the event bus for HTTP response at addresses: {%s}",
+        httpResponseVirtualAddress);
+
+    eventBus.consumer(httpResponseVirtualAddress, this::handleEvent);
+  }
+
+  // Method to handle the event
+  public void handleEvent(Message<String> message) {
+    log.debug("Received HTTP response message: " + message.body());
+  }
+}
