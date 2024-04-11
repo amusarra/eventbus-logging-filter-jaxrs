0a5c79c8e403f631c6a5693e398c843598ac15a3
Antonio Musarra <antonio.musarra@gmail.com>
2024-04-11 17:12:29 +0200

    Step 5 - Registrazione dei Consumer per gli eventi dell'Event Bus (review)

--- src/test/java/it/dontesta/eventbus/publish/PublishMessageOnEventBusTest.java
+++ src/test/java/it/dontesta/eventbus/publish/PublishMessageOnEventBusTest.java
@@ -21,6 +21,9 @@ public class PublishMessageOnEventBusTest {
   @ConfigProperty(name = "app.eventbus.consumer.http.response.address")
   String httpResponseVirtualAddress;
 
+  public static final String HTTP_REQUEST_VIRTUAL_ADDRESS_TEST = "http-request-test";
+  public static final String HTTP_RESPONSE_VIRTUAL_ADDRESS_TEST = "http-response-test";
+
   @Test
   void testPublishMessageOnEventBus() {
     JsonObject requestMessage = new JsonObject().put("message",
@@ -31,15 +34,20 @@ public class PublishMessageOnEventBusTest {
             httpResponseVirtualAddress));
 
     // Register the consumers to the event bus for HTTP request and response
-    // at the addresses defined in the configuration parameters.
+    // at the test addresses.
     // Test the event bus consumers by publishing messages on the event bus
     // to verify that the received message body is the same as the published message.
-    eventBus.consumer(httpRequestVirtualAddress, message -> {
+    eventBus.consumer(HTTP_REQUEST_VIRTUAL_ADDRESS_TEST, message -> {
       assertEquals(requestMessage, message.body());
     });
-    eventBus.consumer(httpResponseVirtualAddress, message -> {
+    eventBus.consumer(HTTP_RESPONSE_VIRTUAL_ADDRESS_TEST, message -> {
       assertEquals(responseMessage, message.body());
     });
+
+    // Publish the messages on the event bus for the HTTP request and response
+    // for the test addresses.
+    eventBus.publish(HTTP_REQUEST_VIRTUAL_ADDRESS_TEST, requestMessage);
+    eventBus.publish(HTTP_RESPONSE_VIRTUAL_ADDRESS_TEST, responseMessage);
 
     // Publish the messages on the event bus for the HTTP request and response
     eventBus.publish(httpRequestVirtualAddress, requestMessage);
