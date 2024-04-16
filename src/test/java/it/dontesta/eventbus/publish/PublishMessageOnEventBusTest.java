db1d9017523eb9f680e4666ff1b5171888ab69d4
Antonio Musarra <antonio.musarra@gmail.com>
2024-04-16 18:25:13 +0200

    Split test in more methods

--- src/test/java/it/dontesta/eventbus/publish/PublishMessageOnEventBusTest.java
+++ src/test/java/it/dontesta/eventbus/publish/PublishMessageOnEventBusTest.java
@@ -1,6 +1,6 @@
 package it.dontesta.eventbus.publish;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.vertx.core.json.JsonObject;
@@ -21,14 +21,40 @@ public class PublishMessageOnEventBusTest {
   @ConfigProperty(name = "app.eventbus.consumer.http.response.address")
   String httpResponseVirtualAddress;
 
+  final String ASSERTION_MESSAGE = "The send message is not as expected";
+
   public static final String HTTP_REQUEST_VIRTUAL_ADDRESS_TEST = "http-request-test";
   public static final String HTTP_RESPONSE_VIRTUAL_ADDRESS_TEST = "http-response-test";
 
   @Test
-  void testPublishMessageOnEventBus() {
+  void testPublishFakeHttpRequestMessageOnEventBus() {
+
     JsonObject requestMessage = new JsonObject().put("message",
         "Message to publish on the event bus {virtualAddress: %s}".formatted(
             httpRequestVirtualAddress));
+
+    // Register the consumers to the event bus for HTTP request and response
+    // at the test addresses.
+    // Test the event bus consumers by publishing messages on the event bus
+    // to verify that the received message body is the same as the published message.
+    eventBus.consumer(HTTP_REQUEST_VIRTUAL_ADDRESS_TEST, message -> {
+      if (message.body().equals(requestMessage)) {
+        message.reply("OK");
+      } else {
+        message.fail(1, "KO");
+      }
+    });
+
+    // Invia il messaggio in modo sincrono e attendi una risposta
+    eventBus.request(HTTP_REQUEST_VIRTUAL_ADDRESS_TEST, requestMessage)
+        .onFailure().invoke(throwable -> {
+          fail(ASSERTION_MESSAGE);
+        }).await().indefinitely();
+  }
+
+  @Test
+  void testPublishFakeHttpResponseMessageOnEventBus() {
+
     JsonObject responseMessage = new JsonObject().put("message",
         "Message to publish on the event {virtualAddress: %s}".formatted(
             httpResponseVirtualAddress));
@@ -37,20 +63,40 @@ public class PublishMessageOnEventBusTest {
     // at the test addresses.
     // Test the event bus consumers by publishing messages on the event bus
     // to verify that the received message body is the same as the published message.
-    eventBus.consumer(HTTP_REQUEST_VIRTUAL_ADDRESS_TEST, message -> {
-      assertEquals(requestMessage, message.body());
-    });
     eventBus.consumer(HTTP_RESPONSE_VIRTUAL_ADDRESS_TEST, message -> {
-      assertEquals(responseMessage, message.body());
+      if (message.body().equals(responseMessage)) {
+        message.reply("OK");
+      } else {
+        message.fail(1, "KO");
+      }
     });
 
-    // Publish the messages on the event bus for the HTTP request and response
-    // for the test addresses.
-    eventBus.publish(HTTP_REQUEST_VIRTUAL_ADDRESS_TEST, requestMessage);
-    eventBus.publish(HTTP_RESPONSE_VIRTUAL_ADDRESS_TEST, responseMessage);
+    // Invia il messaggio in modo sincrono e attendi una risposta
+    eventBus.request(HTTP_RESPONSE_VIRTUAL_ADDRESS_TEST, responseMessage)
+        .onFailure().invoke(throwable -> {
+          fail(ASSERTION_MESSAGE);
+        }).await().indefinitely();
+  }
+
+  @Test
+  void testPublishHttpRequestMessageOnEventBus() {
+
+    JsonObject requestMessage = new JsonObject().put("message",
+        "Message to publish on the event bus {virtualAddress: %s}".formatted(
+            httpRequestVirtualAddress));
 
     // Publish the messages on the event bus for the HTTP request and response
     eventBus.publish(httpRequestVirtualAddress, requestMessage);
-    eventBus.publish(httpResponseVirtualAddress, responseMessage);
   }
+
+  @Test
+    void testPublishHttpResponseMessageOnEventBus() {
+
+        JsonObject responseMessage = new JsonObject().put("message",
+            "Message to publish on the event {virtualAddress: %s}".formatted(
+                httpResponseVirtualAddress));
+
+        // Publish the messages on the event bus for the HTTP request and response
+        eventBus.publish(httpResponseVirtualAddress, responseMessage);
+    }
 }
