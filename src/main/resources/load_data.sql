86fbaa8d6726ecfe71d67746e57fb2f1ef77f315
Antonio Musarra <antonio.musarra@gmail.com>
2024-05-09 10:02:42 +0200

    Modificate insert query per Owner

--- src/main/resources/load_data.sql
+++ src/main/resources/load_data.sql
@@ -7,11 +7,15 @@ INSERT INTO horse(id, name, breed, coat, dateOfBirth) VALUES (3, 'Francisco', 'P
 ALTER SEQUENCE Horse_SEQ RESTART WITH 4;
 
 --  Insert data into the tables Owner
-INSERT INTO owner(id, name, surname, email, phoneNumber) VALUES (1, 'John', 'Doe', 'john.doe@dontesta.it', '123456789');
-INSERT INTO owner(id, name, surname, email, phoneNumber) VALUES (2, 'Mario', 'Rossi', 'mario.rossi@dontesta.it', '987654321');
+INSERT INTO owner(id, name, surname, email, phoneNumber, address, city, state, zipCode, country)
+VALUES (1, 'John', 'Doe', 'john.doe@dontesta.it', '123456789', 'Via Roma 1', 'Rome', 'RM', '00100', 'Italy');
+INSERT INTO owner(id, name, surname, email, phoneNumber, address, city, state, zipCode, country)
+VALUES (2, 'Mario', 'Rossi', 'mario.rossi@dontesta.it', '987654321', 'Via Garibaldi 1', 'Milan', 'MI', '20100',
+        'Italy');
 ALTER SEQUENCE Owner_SEQ RESTART WITH 3;
 
 -- Insert data into the tables Horse_Owner
 INSERT INTO horse_owner(horse_id, owner_id) VALUES (1, 1);
+INSERT INTO horse_owner(horse_id, owner_id) VALUES (1, 2);
 INSERT INTO horse_owner(horse_id, owner_id) VALUES (2, 1);
 INSERT INTO horse_owner(horse_id, owner_id) VALUES (3, 2);
