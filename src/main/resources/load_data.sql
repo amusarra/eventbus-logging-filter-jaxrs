32a8ac4ef67c6c6f23d3f6a1242db6044bbd48e0
Antonio Musarra <antonio.musarra@gmail.com>
2024-05-09 19:07:00 +0200

    Aggiornamento file SQL load_data.sql

--- src/main/resources/load_data.sql
+++ src/main/resources/load_data.sql
@@ -4,7 +4,9 @@
 INSERT INTO horse(id, name, breed, coat, dateOfBirth) VALUES (1, 'Judio XXXV', 'Pura Raza Española - PRE', 'Black', '2012-05-01');
 INSERT INTO horse(id, name, breed, coat, dateOfBirth) VALUES (2, 'Fuego de Cardenas', 'Pura Raza Española - PRE', 'Grey', '2010-03-01');
 INSERT INTO horse(id, name, breed, coat, dateOfBirth) VALUES (3, 'Francisco', 'Puro Sangue Arabo - PSA', 'Grey', '2010-03-01');
-ALTER SEQUENCE Horse_SEQ RESTART WITH 4;
+INSERT INTO horse(id, name, breed, coat, dateOfBirth) VALUES (4, 'Shirus', 'Quarab', 'Grey', '2012-05-01');
+INSERT INTO horse(id, name, breed, coat, dateOfBirth) VALUES (5, 'Artemis', 'San Fratellano', 'Black', '2024-05-01');
+ALTER SEQUENCE Horse_SEQ RESTART WITH 6;
 
 --  Insert data into the tables Owner
 INSERT INTO owner(id, name, surname, email, phoneNumber, address, city, state, zipCode, country)
@@ -12,10 +14,17 @@ VALUES (1, 'John', 'Doe', 'john.doe@dontesta.it', '123456789', 'Via Roma 1', 'Ro
 INSERT INTO owner(id, name, surname, email, phoneNumber, address, city, state, zipCode, country)
 VALUES (2, 'Mario', 'Rossi', 'mario.rossi@dontesta.it', '987654321', 'Via Garibaldi 1', 'Milan', 'MI', '20100',
         'Italy');
-ALTER SEQUENCE Owner_SEQ RESTART WITH 3;
+INSERT INTO owner(id, name, surname, email, phoneNumber, address, city, state, zipCode, country)
+VALUES (3, 'Valentina', 'Rossi', 'valentina.rossi@dontesta.it', '987654321', 'Via Bronte 1', 'Bronte', 'CT', '95034',
+        'Italy');
+ALTER SEQUENCE Owner_SEQ RESTART WITH 4;
 
 -- Insert data into the tables Horse_Owner
-INSERT INTO horse_owner(horse_id, owner_id) VALUES (1, 1);
-INSERT INTO horse_owner(horse_id, owner_id) VALUES (1, 2);
-INSERT INTO horse_owner(horse_id, owner_id) VALUES (2, 1);
-INSERT INTO horse_owner(horse_id, owner_id) VALUES (3, 2);
+INSERT INTO horse_owner(horses_id, owners_id) VALUES (1, 1);
+INSERT INTO horse_owner(horses_id, owners_id) VALUES (1, 2);
+INSERT INTO horse_owner(horses_id, owners_id) VALUES (2, 1);
+INSERT INTO horse_owner(horses_id, owners_id) VALUES (3, 2);
+INSERT INTO horse_owner(horses_id, owners_id) VALUES (4, 1);
+INSERT INTO horse_owner(horses_id, owners_id) VALUES (4, 2);
+INSERT INTO horse_owner(horses_id, owners_id) VALUES (5, 1);
+INSERT INTO horse_owner(horses_id, owners_id) VALUES (5, 3);
