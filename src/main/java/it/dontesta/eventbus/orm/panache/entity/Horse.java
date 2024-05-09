1db6034859cab2cec24da3eca2850ca7f2d1cd2d
Antonio Musarra <antonio.musarra@gmail.com>
2024-05-09 10:03:19 +0200

    Aggiornate entity con @Column annotation

--- src/main/java/it/dontesta/eventbus/orm/panache/entity/Horse.java
+++ src/main/java/it/dontesta/eventbus/orm/panache/entity/Horse.java
@@ -3,6 +3,7 @@ package it.dontesta.eventbus.orm.panache.entity;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -14,9 +15,15 @@ import java.util.List;
 @Cacheable
 public class Horse extends PanacheEntity {
 
+  @Column(length = 60)
   public String name;
+
+  @Column(length = 15)
   public String coat;
+
+  @Column(length = 60)
   public String breed;
+
   public LocalDate dateOfBirth;
 
   @ManyToMany
--- src/main/java/it/dontesta/eventbus/orm/panache/entity/Owner.java
+++ src/main/java/it/dontesta/eventbus/orm/panache/entity/Owner.java
@@ -3,6 +3,7 @@ package it.dontesta.eventbus.orm.panache.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import java.util.List;
@@ -11,14 +12,31 @@ import java.util.List;
 @Cacheable
 public class Owner extends PanacheEntity {
 
+    @Column(length = 60)
     public String name;
+
+    @Column(length = 60)
     public String surname;
+
+    @Column(length = 60, unique = true)
     public String email;
+
+    @Column(length = 20)
     public String phoneNumber;
+
+    @Column(length = 60)
     public String address;
+
+    @Column(length = 60)
     public String city;
+
+    @Column(length = 2)
     public String state;
+
+    @Column(length = 10)
     public String zipCode;
+
+    @Column(length = 60)
     public String country;
 
     @ManyToMany(mappedBy = "owners")
