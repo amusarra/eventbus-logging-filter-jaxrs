f7d5fb347f5e17fb92c47af081fd4678a03ee4f1
Antonio Musarra <antonio.musarra@gmail.com>
2024-05-09 22:12:59 +0200

    Review Unit Test and Integration Test

--- src/test/java/it/dontesta/eventbus/orm/panache/repository/OwnerRepositoryIntegrationTest.java
+++ src/test/java/it/dontesta/eventbus/orm/panache/repository/OwnerRepositoryIntegrationTest.java
@@ -5,15 +5,26 @@ import it.dontesta.eventbus.orm.panache.entity.Owner;
 import jakarta.inject.Inject;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class OwnerRepositoryIntegrationTest {
 
   @Inject
   OwnerRepository ownerRepository;
 
   @Test
+  @Order(1)
+  void testCount() {
+    Assertions.assertEquals(3, ownerRepository.count());
+  }
+
+  @Test
+  @Order(2)
   void testFindById() {
     Owner owner = ownerRepository.findById(1L);
     Assertions.assertNotNull(owner);
@@ -21,12 +32,14 @@ class OwnerRepositoryIntegrationTest {
   }
 
   @Test
+  @Order(3)
   void testFindAll() {
     List<Owner> owners = ownerRepository.listAll();
     Assertions.assertFalse(owners.isEmpty());
   }
 
   @Test
+  @Order(4)
   void testFindOrderedByName() {
     List<Owner> owners = ownerRepository.findOrderedByName();
     Assertions.assertEquals("John", owners.getFirst().name);
@@ -34,6 +47,7 @@ class OwnerRepositoryIntegrationTest {
   }
 
   @Test
+  @Order(5)
   void testFindByName() {
     Owner owner = ownerRepository.find("name", "Mario").firstResult();
     Assertions.assertNotNull(owner);
--- src/test/java/it/dontesta/eventbus/ws/resources/endpoint/repository/v1/HorseRepositoryResourcesTest.java
+++ src/test/java/it/dontesta/eventbus/ws/resources/endpoint/repository/v1/HorseRepositoryResourcesTest.java
@@ -5,64 +5,96 @@ import static org.hamcrest.CoreMatchers.is;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class HorseRepositoryResourcesTest {
 
   @Test
+  @Order(1)
   void getAllHorsesSuccess() {
     given()
         .contentType(ContentType.JSON)
         .when().get("/api/rest/repository/horse/v1")
         .then()
-        .statusCode(200);
+        .statusCode(Response.Status.OK.getStatusCode());
   }
 
   @Test
+  @Order(2)
   void getHorseByIdSuccess() {
     given()
         .contentType(ContentType.JSON)
         .when().get("/api/rest/repository/horse/v1/1")
         .then()
         .body("name", is("Judio XXXV"))
-        .statusCode(200);
+        .statusCode(Response.Status.OK.getStatusCode());
   }
 
   @Test
+  @Order(3)
   void getHorseByIdNotFound() {
     given()
         .contentType(ContentType.JSON)
         .when().get("/api/rest/repository/horse/v1/100")
         .then()
-        .statusCode(404)
+        .statusCode(Response.Status.NOT_FOUND.getStatusCode())
         .body("error", is("Horse with id of 100 not found"));
   }
 
   @Test
+  @Order(4)
   void testCreateHorseBodyNull() {
     given()
         .contentType(ContentType.JSON)
         .when().post("/api/rest/repository/horse/v1")
         .then()
-        .statusCode(400)
+        .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
         .body("title", is("Constraint Violation"));
   }
 
   @Test
+  @Order(5)
+  void testCreateHorse() {
+    String json = """
+        {
+          "name": "Santos XVVI",
+          "coat": "Gray",
+          "breed": "Pura Raza Española - PRE",
+          "dateOfBirth": "2024-05-01",
+          "owners": [{"id": 3}]
+        }
+        """;
+
+    given()
+        .contentType(ContentType.JSON)
+        .body(json)
+        .when().post("/api/rest/repository/horse/v1")
+        .then()
+        .statusCode(Response.Status.CREATED.getStatusCode())
+        .body("name", is("Santos XVVI"));
+  }
+
+  @Test
+  @Order(6)
   void testDeleteHorseByIdSuccess() {
     given()
         .when().delete("/api/rest/repository/horse/v1/1")
         .then()
-        .statusCode(204);
+        .statusCode(Response.Status.NO_CONTENT.getStatusCode());
   }
 
   @Test
+  @Order(7)
   void testDeleteHorseByIdNotFound() {
     given()
         .when().delete("/api/rest/repository/horse/v1/100")
         .then()
-        .statusCode(404);
+        .statusCode(Response.Status.NOT_FOUND.getStatusCode());
   }
-
 }
