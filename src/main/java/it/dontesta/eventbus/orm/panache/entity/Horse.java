a37b3cbdd50d43f458e682f1c18b1253ffa4ee7c
Antonio Musarra <antonio.musarra@gmail.com>
2024-05-09 19:05:45 +0200

    Rimossa annotazione @JoinTable

--- src/main/java/it/dontesta/eventbus/orm/panache/entity/Horse.java
+++ src/main/java/it/dontesta/eventbus/orm/panache/entity/Horse.java
@@ -1,12 +1,9 @@
 package it.dontesta.eventbus.orm.panache.entity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Cacheable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import java.time.LocalDate;
 import java.util.List;
@@ -27,11 +24,5 @@ public class Horse extends PanacheEntity {
   public LocalDate dateOfBirth;
 
   @ManyToMany
-  @JoinTable(
-      name = "horse_owner",
-      joinColumns = @JoinColumn(name = "horse_id"),
-      inverseJoinColumns = @JoinColumn(name = "owner_id")
-  )
-  @JsonManagedReference
   public List<Owner> owners;
 }
